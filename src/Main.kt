0eb8983639be60e2a13bcf97075a2ec002e49775
waimea-znstrange <znstrange@waimea.school.nz>
2026-03-19 12:11:05 +1300

    Name, Author, Repo, Notes

--- src/Main.kt
+++ src/Main.kt
@@ -2,12 +2,12 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
- * GitHub Repo:    GITHUB REPO URL HERE
+ * Project Name:   Chain Reaction
+ * Project Author: Zeb Strange
+ * GitHub Repo:    https://github.com/waimea-znstrange/kotlin-level2-game
  * ---------------------------------------------------------------------
  * Notes:
- * PROJECT NOTES HERE
+ * This is an implementation of the Chain Reaction game
  * =====================================================================
  */
 
