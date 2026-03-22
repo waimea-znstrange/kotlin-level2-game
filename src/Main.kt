f3cf742bb5b7f50953b6dd3a205d10e00d6938c0
waimea-znstrange <znstrange@waimea.school.nz>
2026-03-23 10:59:10 +1300

    set up boxes and ask for the usernames

--- src/Main.kt
+++ src/Main.kt
@@ -11,9 +11,61 @@
  * =====================================================================
  */
 
+// Box Drawings ┻ ┳ ┫ ┣ ┏ ┓ ┛ ┗ ┃ ━
 
+//Global variables
+val boxes = mutableListOf<String>()
+val empty = " ⃠ "
 
 fun main() {
-    println("Hello World!")
+    //Heading
+    println("┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+    println("┃  Chain Reaction Game  ┃")
+    println("┗━━━━━━━━━━━━━━━━━━━━━━━┛")
+
+    getUserNames()
+    createBoxes()
+    showBoxes()
+}
+
+fun createBoxes() {
+    repeat(12) {
+        boxes.add(empty)
+    }
+}
+
+fun showBoxes(){
+    //Box labels
+    for (i in 1..boxes.size) {
+        print("  $i  ")
+    }
+    println()
+
+    //Top border
+    println("┏━━━━" + "┳━━━━".repeat(boxes.size - 1) + "┓")
+
+    //Box contents
+    for (box in boxes){
+        print("┃ ${box.padEnd(4)}")
+    }
+    println("┃")
+
+    //Bottom border
+    println("┗━━━━" + "┻━━━━".repeat(boxes.size - 1) + "┛")
+}
+
+fun getUserNames() {
+    var userName1: String
+    while (true) {
+        println("Player 1: ")
+        userName1 = readln()
+        if (userName1.isNotBlank()) break
+    }
+    var userName2: String
+    while (true) {
+        println("Player 2: ")
+        userName2 = readln()
+        if (userName2.isNotBlank()) break
+    }
 }
 
