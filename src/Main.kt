e6d56f73fb6b0679f0494149047ccdf4a8c10e58
waimea-znstrange <znstrange@waimea.school.nz>
2026-03-24 15:05:13 +1300

    added an option to pick symbols for player 1 & 2

--- src/Main.kt
+++ src/Main.kt
@@ -11,28 +11,37 @@
  * =====================================================================
  */
 
-// Box Drawings ┻ ┳ ┫ ┣ ┏ ┓ ┛ ┗ ┃ ━
+// Box Drawings ┻ ┳ ┫ ┣ ┏ ┓ ┛ ┗ ┃ ━   ⃠
+// Player 1: ♥♦♠♣
+// Player 2: ♡♢♤♧
 
 //Global variables
 val boxes = mutableListOf<String>()
-val empty = " ⃠ "
+val empty = ""
+
 
 fun main() {
     //Heading
     println("┏━━━━━━━━━━━━━━━━━━━━━━━┓")
     println("┃  Chain Reaction Game  ┃")
+    println("┃  By Zeb Strange       ┃")
     println("┗━━━━━━━━━━━━━━━━━━━━━━━┛")
 
     getUserNames()
     createBoxes()
     showBoxes()
+//    getMove()
+//    makeMove()
+//    checkForWin()
 }
+
 
 fun createBoxes() {
     repeat(12) {
         boxes.add(empty)
     }
 }
+
 
 fun showBoxes(){
     //Box labels
@@ -46,7 +55,7 @@ fun showBoxes(){
 
     //Box contents
     for (box in boxes){
-        print("┃ ${box.padEnd(4)}")
+        print("┃ ${box.padEnd(3)}")
     }
     println("┃")
 
@@ -54,18 +63,80 @@ fun showBoxes(){
     println("┗━━━━" + "┻━━━━".repeat(boxes.size - 1) + "┛")
 }
 
+
 fun getUserNames() {
+    // Ask for player 1 name
     var userName1: String
     while (true) {
-        println("Player 1: ")
+        print("Player 1 name: ")
         userName1 = readln()
+        println()
         if (userName1.isNotBlank()) break
     }
+
+    // Ask for player 1 symbol
+    println("Pick a symbol:")
+    println("[1]♥")
+    println("[2]♦")
+    println("[3]♠")
+    println("[4]♣")
+    println("Choice: ")
+
+    var symbol1 = readln()
+
+    when {
+        symbol1 == "1" -> symbol1 = "♥"
+        symbol1 == "2" -> symbol1 = "♦"
+        symbol1 == "3" -> symbol1 = "♠"
+        symbol1 == "4" -> symbol1 = "♣"
+    }
+    println("$userName1 chose $symbol1 as their symbol")
+    println()
+    println()
+
+
+    // Ask for player 2 name
     var userName2: String
     while (true) {
-        println("Player 2: ")
+        print("Player 2 name: ")
         userName2 = readln()
+        println()
         if (userName2.isNotBlank()) break
     }
+
+    // Ask for player 2 symbol
+    println("Pick a symbol:")
+    println("[1]♡")
+    println("[2]♢")
+    println("[3]♤")
+    println("[4]♧")
+    println("Choice: ")
+
+    var symbol2 = readln()
+
+    when {
+        symbol2 == "1" -> symbol2 = "♡"
+        symbol2 == "2" -> symbol2 = "♢"
+        symbol2 == "3" -> symbol2 = "♤"
+        symbol2 == "4" -> symbol2 = "♧"
+    }
+    println("$userName2 chose $symbol2 as their symbol")
+    println()
+    println()
 }
 
+//
+//
+//fun getMove() {
+//
+//}
+//
+//
+//fun makeMove() {
+//
+//}
+//
+//
+//fun checkForWin() {
+//
+//}
